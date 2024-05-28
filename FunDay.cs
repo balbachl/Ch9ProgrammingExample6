405db5f78ab4dac380f095e47565e3d2cd1ec33b
balbachl <lisajbalbach@gmail.com>
2024-05-28 14:29:50 -0400

    Add project files.

---
+++ FunDay.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace PlayGround
+{
+    class FunDay
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public int Age { get; set; }
+        private bool _elementaryPlayground;
+        public bool ElementaryPlayground
+        {
+            get
+            {
+                return _elementaryPlayground;
+            }
+            set
+            {
+                if (Age < 5)
+                    _elementaryPlayground = false;
+                else
+                    _elementaryPlayground = true;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using PlayGround;
+namespace ElementaryUse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FunDay child = new FunDay();
+            Console.WriteLine("Please enter the child\'s first name: ");
+            child.FirstName = Console.ReadLine();
+            Console.WriteLine("Please enter the child\'s last name: ");
+            child.LastName = Console.ReadLine();
+            int tempAge;
+            Console.WriteLine($"Please enter the child\'s age to the nearest year");
+            while(!int.TryParse(Console.ReadLine(), out tempAge))
+            {
+                Console.WriteLine("Please enter a valid number without decimal places");
+            }
+            child.Age = tempAge;
+            if(child.ElementaryPlayground)
+                Console.WriteLine($"{child.FirstName} {child.LastName} " +
+                    $" is old enough for the elementary playground.");
+            else
+                Console.WriteLine($"{child.FirstName} {child.LastName} " +
+                    $" may use the preschool playground instead of the " +
+                    $"elementary playground.");
+        }
+    }
+}
